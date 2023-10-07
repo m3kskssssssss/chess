8fdf0b63bdc8ecc63be1d5e1d362eacefc00af66
Mickle <misha_2003_5@mail.ru>
2023-10-07 15:54:27 +0500

    Заброшены шахматы с интерфейсом, сделаны пункты А и Б без интерфейса
    Понял что не хватит времени чтобы разобраться с интерфейсом для шахмат и их управлением.

--- chess.py
+++ chess.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
+from functools import *
 
+button_ID = []
 #СОЗДАЕМ РОДИТЕЛЬСКОЕ ОКНО
 root = tk.Tk()
 root.title("CHESS")
@@ -8,20 +10,18 @@ root.geometry('1000x600')
 tabControl = ttk.Notebook(root)
 
 #СОЗДАЕМ ВКЛАДКИ ДЛЯ ПЕРЕКЛЮЧЕНИЯ
-test_a = ttk.Frame(tabControl)
-test_b = ttk.Frame(tabControl)
-test_c = ttk.Frame(tabControl)
+test_a = tk.Frame(tabControl)
+test_b = tk.Frame(tabControl)
+test_c = tk.Frame(tabControl)
 
 tabControl.add(test_a, text='TEST A')
 tabControl.add(test_b, text='TEST B')
 tabControl.add(test_c, text='TEST C')
 tabControl.pack(expand=0, fill="both")
 
-
-#СОЗДАЕМ КЛИКАБЕЛЬНЫЕ КЛЕТОЧКИ
 for xA in range(8):
     for yA in range(8):
-        if (xA + yA) % 2 == 1:
+        if (xA + yA) % 2 != 1:
             btn = tk.Button(test_a ,text='', width= 10, height= 4, bg='black',  )
             btn.grid(row=xA, column=yA)
         else:
@@ -30,7 +30,7 @@ for xA in range(8):
 
 for xB in range(8):
     for yB in range(8):
-        if (xB + yB) % 2 == 1:
+        if (xB + yB) % 2 != 1:
             btn = tk.Button(test_b ,text='', width= 10, height= 4, bg='black',  )
             btn.grid(row=xB, column=yB)
         else:
@@ -39,15 +39,12 @@ for xB in range(8):
 
 for xC in range(8):
     for yC in range(8):
-        if (xC + yC) % 2 == 1:
+        if (xC + yC) % 2 != 1:
             btn = tk.Button(test_c ,text='', width= 10, height= 4, bg='black',  )
             btn.grid(row=xC, column=yC)
         else:
             btn = tk.Button(test_c ,text="", width= 10, height= 4, bg='white',   )
             btn.grid(row=xC, column=yC)
-
-
-
 
 
 root.mainloop()
